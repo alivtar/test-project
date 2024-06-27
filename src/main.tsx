d75df8c3fc2e71b332d59f373cae6c3eb8798c91
Ali Baghban <ali.angori007@gmail.com>
2024-06-27 17:54:54 +0330

    added support for sass styles in project

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
-import "./index.css"
+import "./index.sass"
 import store from "./app/store"
 
 const container = document.getElementById("root")
